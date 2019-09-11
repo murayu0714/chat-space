0a863475445badd909f9df83f4d49d0bcd70c15a
murayu0714 <ownlife0714@gmail.com>
2019-09-12 02:18:34 +0900

    If statement change

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,49 +1,35 @@
 $(function(){
 
   function buildHTML(message){
-    if ( message.image ) {
-      var html =
+    var content = message.content ? `${ message.content }` : "";
+    var image = message.image ? `<img src= ${ message.image }>`: "";
+
+    var html =
       `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.date}
-            </div>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
           </div>
+          <div class="upper-message__date">
+            ${message.date}
+          </div>
+        </div>
           <div class="lower-message">
             <p class="lower-message__content">
               ${message.content}
             </p>
+            ${image}
           </div>
-          <img src=${message.image} >
-        </div>`
+      </div>`
       return html;
-    } else {
-      var html =
-      `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.date}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
-    };
   }
+  
   $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
   var url = $(this).attr('action')
+
+
   $.ajax({
     url: url,
     type: "POST",
@@ -52,15 +38,17 @@ $(function(){
     processData: false,
     contentType: false
   })
+
   .done(function(data){
     var html = buildHTML(data);
     $('.messages').append(html);
     $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
     $('form')[0].reset();
   })
-    .fail(function(){
-      alert('error');
-    });
+
+  .fail(function(){
+    alert('error');
+  });
     return false;
   });
 });
