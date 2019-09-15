6d9996f85faf0666801e223d118693c333ca2c7d
murayu0714 <ownlife0714@gmail.com>
2019-09-15 15:14:06 +0900

    create-auto-update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -27,41 +27,40 @@ $(function(){
   // *非同期通信
 
   $('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
 
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
 
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);
-    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
-    $('form')[0].reset();
-  })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
+      $('form')[0].reset();
+    })
 
-  .fail(function(){
-    alert('error');
-  })
+    .fail(function(){
+      alert('error');
+    })
 
-  .always(function(){
-    $('.form__submit').prop('disabled', false);
+    .always(function(){
+      $('.form__submit').prop('disabled', false);
+    });
   });
-  });
-
-  //  ＊自動更新
+  
+  //  自動更新
 
   var reloadMessages = function(){
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
       var last_message_id = $('.message').last().data('message-id')
-      // $('.message:last').data("message-id");
       
       $.ajax({
         url: "api/messages",
@@ -82,73 +81,7 @@ $(function(){
       .fail(function(){
         alert('自動更新に失敗しました');
       });
-
     }
   };
   setInterval(reloadMessages, 5000);
 });
-
-
-//   var interval = setInterval(function(){
-//     if (window.location.href.match(/\/groups\/\d+\/messages/)){
-//       var last_message_id = $('.main__message__box').filter(":last").data('messageId')
-
-//       $.ajax({
-//         url: location.href.json,
-//         data: { last_id: last_message_id },
-//         type: "GET",
-//         dataType: 'json'
-//       })
-
-//       .done(function(data){
-//         var insertHTML = '';
-//         data.forEach(function(message){
-//           insertHTML = buildHTML(message);         
-//           $('.main__message').append(insertHTML)
-//           ScrollToNewMessage();
-//         });
-//       })
-
-//       .fail(function(data){
-//         alert('自動更新に失敗しました');
-//       });
-//     }
-//   }
-// });
-
-//  ＊自動更新
-
-// var reloadMessages = function() {
-// if (window.location.href.match('/\/groups\/\d+\/messages/')){
-// var last_message_id = $('.message:last').data("message-id");
-// //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-
-// $.ajax({
-// //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-// url: "api/messages",
-// //ルーティングで設定した通りhttpメソッドをgetに指定
-// type: 'get',
-// data: {id: last_message_id}
-// dataType: 'json',
-// //dataオプションでリクエストに値を含める
-// })
-
-// .done(function(messages) {
-// var insertHTML = '';
-// messages.forEach(function(message){
-// insertHTML = buildHTML(message);
-// $('.messages').append(insertHTML);
-// })
-// $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
-// // $('form')[0].reset();
-// })
-// .fail(function() {
-// alert('自動更新失敗');
-// });
-// }
-// };
-// setInterval(reloadMessages, 5000);
-// });
-
-
-// $('.message').filter(":last").data('message-id')
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,13 +1,5 @@
-# json.id @message.id
-# json.user_name @message.user.name
-# # json.date @message.created_at.strftime("%Y/%m/%d %H:%M")
-# json.date @message.created_at.to_s
-json.content @message.content
-json.image @message.image_url
-
 json.id @message.id
 json.user_name @message.user.name
 json.date @message.created_at.to_s
-# json.messages (@message, :content, :image)
-
-
+json.content @message.content
+json.image @message.image_url
