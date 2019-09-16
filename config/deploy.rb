47bea45968e1b654b427c0390dab19298f96cfb8
murayu0714 <ownlife0714@gmail.com>
2019-09-16 20:46:23 +0900

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 'chat-space'
+lock '3.11.1'
 
 # Capistranoのログの表示に利用する
 set :application, 'chat-space'
